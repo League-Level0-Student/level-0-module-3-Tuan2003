b8730b0ac97447bed2e17ca606550cd0588c94b5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-10 14:25:41 -0700

    HighLowGame,BirthdayReminder,CrazyCatLady, RobotColorChooser

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,14 +11,25 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "June 12th";
+		String dadsBirthday = "June 21st";
+		String myBirthday = "October 9th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+String response = JOptionPane.showInputDialog("Which birthday do you want to know? Mom's, Dad's or Me?");
 		// 3. Print out what the user typed
-		
+System.out.println(response);
+if(response.equalsIgnoreCase("mom")) {
+JOptionPane.showMessageDialog(null, momsBirthday);	
+}
+if(response.equalsIgnoreCase("dad")){
+JOptionPane.showMessageDialog(null, dadsBirthday);	
+}
+if(response.equalsIgnoreCase("Tuan")) {
+JOptionPane.showMessageDialog(null, myBirthday);	
+}else {
+JOptionPane.showMessageDialog(null, "Sorry, I don't remember that person's birthday.");
+}
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -8,32 +8,59 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
+
 	public static void main(String[] args) {
 Robot Trump = new Robot();		
 Trump.setSpeed(1000);
 Trump.miniaturize();
 Trump.penDown();
 for (int i = 0; i < 4; i++) {
+String response = JOptionPane.showInputDialog("What color would you like the robot to draw with?");
+
+
+if (response.equalsIgnoreCase("blue")) {
+	Trump.setPenColor(Color.blue);
+}
+if (response.equalsIgnoreCase("yellow")) {
+	Trump.setPenColor(Color.yellow);	
+}
+if (response.equalsIgnoreCase("black")) {
+	Trump.setPenColor(Color.black);	
+}
+if (response.equalsIgnoreCase("orange")) {
+	Trump.setPenColor(Color.orange);	
+}
+if (response.equalsIgnoreCase("green")) {
+	Trump.setPenColor(Color.green);	
+}
+if (response.equalsIgnoreCase("magenta")) {
+	Trump.setPenColor(Color.magenta);	
+}
+if (response.equalsIgnoreCase("gray")) {
+	Trump.setPenColor(Color.gray);	
+}
+if (response.equalsIgnoreCase("cyan")) {
+	Trump.setPenColor(Color.cyan);	
+}
+if (response.equalsIgnoreCase("pink")) {
+	Trump.setPenColor(Color.pink);
+
+}
 Trump.move(100);
 Trump.turn(90);	
-}
-
-//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+//1. Create a new Robot		
+//3. Ask the user what color they would like the robot to draw		
+//5. Use an if/else statement to set the pen color that the user requested
+//6. If the user doesn’t enter anything, choose a random color
+//7. Put a loop around your code so that you keep asking the user for more colors & drawing them		
+//4. Set the pen width to 10		
+//2. Make the robot draw a shape (this will take more than one line of code)
 
 
 	}
 }
+	}
+
