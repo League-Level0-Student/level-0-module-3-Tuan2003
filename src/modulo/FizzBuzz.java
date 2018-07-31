646b19f56b9c2528e2ebc0e67f3e4f885bfcdff4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-31 14:24:57 -0700

    FizzBuzz

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -28,13 +28,16 @@ package modulo;
 public class FizzBuzz {
 	
 public static void main(String[] args) {
-for (int i = 0; i < 100; i++) {
-	if(i%3==0) {
-		System.out.println(i);
-	}else if(i%2==0) {
-		System.out.println("buzz");
-	}else {
+for (int i = 1; i < 100; i++) {
+	if(i%3==0&&i%5==0) {
+		System.out.println("fizzbuzz");
+	
+	}else if(i%5==0) {
+			System.out.println("buzz");
+	}else if(i%3==0) {
 		System.out.println("fizz");
+	}else {
+		System.out.println(i);
 	}
 }
 
