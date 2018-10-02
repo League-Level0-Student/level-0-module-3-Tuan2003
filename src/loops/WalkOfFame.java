3d3531340c803e8a74e17b90ce316a6423feb718
313509 <313509@0338S-S-R0B6H0H.sdcs.local>
2018-10-02 13:48:07 -0700

    yearsAlive, WalkOfFame

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -1,37 +1,36 @@
+package loops;
+
 
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-package loops;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
+	
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+	Robot rob = new Robot();	
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		
-		rob.setX(10);
-		rob.setY(150);
 		rob.penDown();
+		rob.miniaturize();		
+		
+		for (int i = 0; i < 15; i++) {
+		
+			rob.setX(i*50);
+		rob.setY(i*50);
 		rob.setSpeed(100);
-		rob.miniaturize();
-		
-		for (int i = 0; i <= 5; i++) {
+		for (int l = 0; l < 5; l++) {
 		rob.move(30);
-		rob.turn(144);	
-			
+		rob.turn(144);
+		
+		}	
 		}
-		
-		
-		
 		// You also need to show the robot to see the result of this line.
-
 		// 2. Make the robot draw a star shape. Hint: 144.
-
 		// 3. Set the size of the star to 30.
-
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
@@ -41,5 +40,6 @@ public class WalkOfFame {
 		 */
 
 	}
+
 
 }
---
+++ src/yearsAlive.java
@@ -0,0 +1,17 @@
+
+public class yearsAlive {
+public static void main(String[] args) {
+
+for (int i = 2003; i <= 2018; i++) {
+System.out.println(i);
+}	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+}
